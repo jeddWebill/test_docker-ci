1d91bc68e576c514a969c76c7bbeb40331a14806
jeddPdev <jedd@pdev.co.za>
2017-08-25 10:37:48 +0200

    Added test service

---
+++ Docker-Test-Services/server.js
@@ -0,0 +1,37 @@
+const Hapi = require('hapi');
+var mongodb = require('mongodb');
+var client = mongodb.MongoClient;
+
+var uri = "mongodb://mongo/test";
+const server = new Hapi.Server();
+server.connection({ port: 8080 });
+server.route(
+    {method: 'GET',
+    path: '/db',
+    handler:function(req,reply) {
+    client.connect(uri, function (err, db) {
+        if (err) reply( res.json(err));
+        var collection = db.collection('dummy');
+        collection.find({}).toArray(function(err, docs) {
+            if (err) reply(err);
+            reply("Connected to db fine");
+        });
+    })}});
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('This server is running');
+    }
+});
+
+server.start((err) => {
+
+    if (err)
+    throw err;
+
+console.log('Server running at:', server.info.uri);
+});
+/**
+ * Created by User on 2017-08-16.
+ */
